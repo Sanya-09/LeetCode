61789a2b5def392c9a36b26fbdaa575b50e3fbaf
Sanya Agarwal <agarwalsanya90@gmail.com>
2026-01-15 00:26:16 +0530

    Create removeduplicates_set.java

---
+++ Solution/26-remove-duplicates-from-sorted-array/removeduplicates_set.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int removeDuplicates(int[] nums) {
+        // set has no duplicates 
+        HashSet<Integer> st = new HashSet<>();
+        int unique = 0;
+        for(int num: nums){
+            if(!st.contains(num)){
+                st.add(num);
+                nums[unique] = num;
+                unique ++;
+            }
+        }
+        return unique;
+    }
+}
