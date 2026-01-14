33fede095b434b2fe41e20b056e97fe555e3af8c
Sanya Agarwal <agarwalsanya90@gmail.com>
2026-01-14 23:07:45 +0530

    Time: 0 ms (100.00%) | Memory: 43.6 MB (31.06%) - LeetSync

---
+++ Solution/66-plus-one/plus-one.java
@@ -0,0 +1,22 @@
+class Solution {
+    public int[] plusOne(int[] digits) {
+
+        // Start from the last digit
+        for (int i = digits.length - 1; i >= 0; i--) {
+
+            // If digit is less than 9, just add 1 and return
+            if (digits[i] < 9) {
+                digits[i]++;
+                return digits;
+            }
+
+            // If digit is 9, make it 0 and continue carry
+            digits[i] = 0;
+        }
+
+        // If all digits were 9, we need an extra digit
+        int[] result = new int[digits.length + 1];
+        result[0] = 1;   // remaining digits are already 0
+        return result;
+    }
+}
