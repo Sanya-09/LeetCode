fc343b889fe0deed16a7f5b3c42a50eb6fcfed30
Sanya Agarwal <agarwalsanya90@gmail.com>
2026-01-16 09:19:15 +0530

    Time: 140 ms (5.04%) | Memory: 48 MB (11.10%) - LeetSync

---
+++ Solution/283-move-zeroes/move-zeroes.java
@@ -0,0 +1,19 @@
+class Solution {
+    public void moveZeroes(int[] nums) {
+        int n = nums.length;
+        int[] temp = new int[n];
+        int index = 0;
+        for(int i = 0 ; i < n ; i++){
+            if(nums[i] != 0){
+                temp[index] = nums[i];
+                index++;
+            }
+        }
+        for(int i = 0 ; i < n ; i++){
+            nums[i] = temp[i];
+        }
+        for(int num : nums){
+            System.out.print(num+" ");
+        }
+    }
+}
