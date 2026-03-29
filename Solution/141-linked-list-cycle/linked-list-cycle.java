2e338b7f52decbf0c71f800226cf00495702659c
Sanya Agarwal <agarwalsanya90@gmail.com>
2026-03-29 20:03:53 +0530

    Time: 0 ms (100.00%) | Memory: 47.1 MB (10.26%) - LeetSync

--- Solution/141-linked-list-cycle/linked-list-cycle.java
+++ Solution/141-linked-list-cycle/linked-list-cycle.java
@@ -14,8 +14,10 @@ public class Solution {
         if(head == null || head.next == null) return false;
         ListNode slow = head;
         ListNode fast = head;
-        while(fast != null && fast.next != null){
+        while(fast != null){
+            if(slow==null) return false;
             slow = slow.next;
+            if(fast.next == null) return false;
             fast = fast.next.next;
             if(slow == fast) return true;
         }
