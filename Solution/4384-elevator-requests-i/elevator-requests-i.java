74f18abec95c4d4230e859bf2cdb45ea3b319ad9
Sanya Agarwal <agarwalsanya90@gmail.com>
2026-08-17 00:20:03 +0530

    Time: 1 ms (100.00%) | Memory: 44.7 MB (100.00%) - LeetSync

--- Solution/4384-elevator-requests-i/elevator-requests-i.java
+++ Solution/4384-elevator-requests-i/elevator-requests-i.java
@@ -3,12 +3,9 @@ class Solution {
         int prefloor = 0;
 
         int ans=0;
-
-
         for(int i=0 ; i<requests.length ; i++){
             
             int currentfloor = requests[i];
-
             ans = ans + Math.abs(currentfloor - prefloor);
             prefloor=currentfloor;
         }
